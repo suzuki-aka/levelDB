60488aee1cb378e54546cf609a3cc4063c3a2e4a
AnhNC-ThinkPad <anhnc.com@gmail.com>
2018-01-13 23:30:33 +0700

    Create draft some function

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-const level = require('level');
+const LevelDB = require('./lib/db');
+var db = new LevelDB('./db');
 
-var db = level('./db', { valueEncoding: 'json', keyEncoding: 'json' });
-
-db.batch()
-    .put({ id: 0, table: 'article' }, { 'firstName': 'Anh', 'lastName': 'Nguyen' }) // 
-    .put({ id: 10, table: 'article' }, { 'firstName': 'Anh', 'lastName': 'Nguyen' }) // 
-    .put('ten', { 'firstName': 'Anh', 'lastName': 'Nguyen' }) // 
-    .write(() => console.log('Done'));
-
-db.get('ten', function (err, value) {
-    if (err) console.log(err);
-    console.log(value);
-});
+db.use('article');
+db.insert([
+    { id: '10', name: '1000' },
+    { id: '11', name: '2132432' },
+]);
+db.get();
+console.log(db.listRecords);
